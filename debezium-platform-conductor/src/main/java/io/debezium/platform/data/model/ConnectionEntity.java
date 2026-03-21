f591e428a946635f465ee32dd8beefc92f6df31e
ffccites <99155080+PDGGK@users.noreply.github.com>
2026-03-21 19:40:56 +1100

    debezium/dbz#1245 Add description field to connections API payload
    Signed-off-by: Zihan Dai <dzh1436286758@gmail.com>

--- debezium-platform-conductor/src/main/java/io/debezium/platform/data/model/ConnectionEntity.java
+++ debezium-platform-conductor/src/main/java/io/debezium/platform/data/model/ConnectionEntity.java
@@ -31,6 +31,8 @@ public class ConnectionEntity {
     @Column(unique = true, nullable = false)
     private String name;
 
+    private String description;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Type type;
@@ -85,6 +87,14 @@ public class ConnectionEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Type getType() {
